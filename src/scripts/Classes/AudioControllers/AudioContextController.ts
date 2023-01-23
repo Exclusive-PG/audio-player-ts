ec46d3c5ffb05c7bc9508768912940b151ec91f3
Exclusive-PG <dimchik_231@mail.ru>
2023-01-23 21:07:40 +0200

    added gain node

--- src/scripts/Classes/AudioControllers/AudioContextController.ts
+++ src/scripts/Classes/AudioControllers/AudioContextController.ts
@@ -2,23 +2,30 @@ export default class AudioContextController {
 	private audioContext: AudioContext;
 	private analyzer: AnalyserNode;
 	private source: MediaElementAudioSourceNode;
-
+    private gainNode :  GainNode;
 	public init(audio: HTMLAudioElement) {
 		if (!this.audioContext) {
 			this.audioContext = new AudioContext();
 			this.analyzer = this.audioContext.createAnalyser();
 			this.analyzer.fftSize = 8192;
 			this.source = this.audioContext.createMediaElementSource(audio);
-            console.log("audio context created");
+			this.gainNode = this.audioContext.createGain();
+			this.gainNode.gain.value = 0.1; 
+			console.log("AudioContextController created");
 		} else {
-			console.log("audio context already create");
+			console.log("AudioContextController already create");
 		}
 	}
-    public connectPath(){
-        this.source.connect(this.analyzer);
-        this.analyzer.connect(this.audioContext.destination);
-    }
+	public connectPath() {
+		this.source.connect(this.analyzer);
+        this.source.connect(this.gainNode);
+		this.analyzer.connect(this.audioContext.destination);
+        this.gainNode.connect(this.audioContext.destination);
+	}
 	get getAnalyzer(): AnalyserNode {
 		return this.analyzer;
+	}
+	set setFftSize(value: number) {
+		this.analyzer.fftSize = value;
 	}
 }
